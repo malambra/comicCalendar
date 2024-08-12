d80af368b7b987841ff04cf890978893abd1d02b
Manuel Alambra Giner <manuelalambraginer@MacBook-Air-de-Manuel.local>
2024-08-12 15:13:19 +0200

    feat: enrich scripts and enrich events.json

--- app/models/events.py
+++ app/models/events.py
@@ -8,6 +8,9 @@ class Event(BaseModel):
     start_date: str
     end_date: str
     province: str
+    community: str
+    city: str
+    type: str
     address: str
     description: str
 
@@ -17,6 +20,9 @@ class EventMod(BaseModel):
     start_date: Optional[str] = None
     end_date: Optional[str] = None
     province: Optional[str] = None
+    community: Optional[str] = None
+    city: Optional[str] = None
+    type: Optional[str] = None
     address: Optional[str] = None
     description: Optional[str] = None
 
